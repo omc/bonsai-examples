8d7ec840bd4a786648bbd25eb55afb70e4c29be8
Rob Sears <secure@robsears.com>
2019-10-17 09:30:40 -0500

    Minor refactoring

---
+++ rails/app/controllers/search_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class SearchController< ApplicationController
+  def run
+    @results = Elasticsearch::Model.search(params[:q]).records
+  end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/search', to: 'application#search'
+  post '/search', to: 'search#run'
 end
