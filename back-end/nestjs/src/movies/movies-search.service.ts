0631b9282fdab95aec8cf18a5cb02216c5cca4d4
Mo Omer <mo@ipv7.com>
2024-09-05 22:20:23 -0500

    Add MoviesSearchService

--- back-end/nestjs/src/movies/movies-search.service.ts
+++ back-end/nestjs/src/movies/movies-search.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { WriteResponseBase } from '@elastic/elasticsearch/lib/api/types';
-import { Post } from './entities/post.entity';
-import { PostSearchDocument } from './types/post-search-document.interface';
+import { MovieEntity } from './infrastructure/persistence/relational/entities/movie.entity';
+import { MovieSearchDocument } from './interfaces/movie-search-document.interface';
 
 @Injectable()
-export class PostsSearchService {
-  index = 'posts';
+export class MoviesSearchService {
+  index = 'movies';
 
   constructor(private readonly elasticsearchService: ElasticsearchService) {}
 
-  async indexPost(post: Post): Promise<WriteResponseBase> {
-    return this.elasticsearchService.index<PostSearchDocument>({
+  async indexMovie(movie: MovieEntity): Promise<WriteResponseBase> {
+    return await this.elasticsearchService.index<MovieSearchDocument>({
       index: this.index,
       document: {
-        id: post.id,
-        title: post.title,
-        paragraphs: post.paragraphs,
-        authorId: post.author.id,
+        id: movie.id,
+        title: movie.title,
       },
     });
   }
@@ -40,10 +38,10 @@ export class PostsSearchService {
   async search(text: string, offset?: number, limit?: number, startId = 0) {
     let separateCount = 0;
     if (startId) {
-      separateCount = await this.count(text, ['title', 'paragraphs']);
+      separateCount = await this.count(text, ['title']);
     }
-    const { hits } = await this.elasticsearchService.search<PostSearchDocument>(
-      {
+    const { hits } =
+      await this.elasticsearchService.search<MovieSearchDocument>({
         index: this.index,
         from: offset,
         size: limit,
@@ -52,7 +50,7 @@ export class PostsSearchService {
             should: {
               multi_match: {
                 query: text,
-                fields: ['title', 'paragraphs'],
+                fields: ['title'],
               },
             },
             filter: {
@@ -64,52 +62,12 @@ export class PostsSearchService {
             },
           },
         },
-      },
-    );
+      });
     const count = hits.total;
     const results = hits.hits.map((item) => item._source);
     return {
       count: startId ? separateCount : count,
       results,
     };
-  }
-
-  async remove(postId: number) {
-    this.elasticsearchService.deleteByQuery({
-      index: this.index,
-      query: {
-        match: {
-          id: postId,
-        },
-      },
-    });
-  }
-
-  async update(post: Post) {
-    const newDocument: PostSearchDocument = {
-      id: post.id,
-      title: post.title,
-      paragraphs: post.paragraphs,
-      authorId: post.author.id,
-    };
-
-    const script = Object.entries(newDocument).reduce(
-      (result, [key, value]) => {
-        return `${result} ctx._source.${key}='${value}';`;
-      },
-      '',
-    );
-
-    return this.elasticsearchService.updateByQuery({
-      index: this.index,
-      script: {
-        source: script,
-      },
-      query: {
-        match: {
-          id: post.id,
-        },
-      },
-    });
   }
 }
